af913370aec71208ede2f1790c44e4ee34b6b200
leejunho0622 <lee49409780@gmail.com>
2024-11-11 16:40:41 +0900

    Add Zombie constructor and Attack method

--- zombie/src/Monster/Zombie.java
+++ zombie/src/Monster/Zombie.java
@@ -1,5 +1,23 @@
 package Monster;
 
-public class Zombie {
+import java.util.Random;
 
+import main.Unit;
+
+public class Zombie extends Unit {
+	private int power;
+	
+	public Zombie(int hp, int mp, int level, int exp, int position) {
+		super(hp, mp, level, exp, position);
+	}
+	
+	public void attack(Unit player) {
+		Random ran = new Random();
+		power = ran.nextInt(this.level*2)+1;
+		player.setHp(player.getHp() - power);
+		if(player.getHp() < 0)
+			player.setHp(0);
+		
+		System.out.println("좀비가 "+power+"만큼 공격!");
+	}
 }
