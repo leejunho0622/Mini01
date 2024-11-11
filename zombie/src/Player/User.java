0ff5118e7c2e1ee91ebb0d5ee54a6dbd638c4089
leejunho0622 <lee49409780@gmail.com>
2024-11-11 16:50:20 +0900

    Add User constructor and attack method

--- zombie/src/Player/User.java
+++ zombie/src/Player/User.java
@@ -1,5 +1,23 @@
 package Player;
 
-public class User {
+import java.util.Random;
 
+import main.Unit;
+
+public class User extends Unit{
+	private int power;
+	
+	public User(int hp, int mp, int level, int exp, int position) {
+		super(hp, mp, level, exp, position);
+	}
+	
+	public void attack(Unit monster) {
+		Random ran = new Random();
+		power = ran.nextInt(this.level*2)+1;
+		monster.setHp(monster.getHp() - power);
+		if(monster.getHp() < 0)
+			monster.setHp(0);
+		
+		System.out.println("플레이어가 "+power+"만큼 공격!");
+	}
 }
