cd3306e6dc98b842c76766bb0781bd100b30ddeb
leejunho0622 <lee49409780@gmail.com>
2024-11-11 17:02:50 +0900

    Add Skeleton constructor and attack method

--- zombie/src/Monster/Skeleton.java
+++ zombie/src/Monster/Skeleton.java
@@ -1,5 +1,23 @@
 package Monster;
 
-public class Skeleton {
+import java.util.Random;
 
+import main.Unit;
+
+public class Skeleton extends Unit{
+	private int power;
+	
+	public Skeleton(int hp, int mp, int level, int exp, int position) {
+		super(hp, mp, level, exp, position);
+	}
+	
+	public void attack(Unit player) {
+		Random ran = new Random();
+		power = ran.nextInt(this.level*3)+1;
+		player.setHp(player.getHp() - power);
+		if(player.getHp() < 0)
+			player.setHp(0);
+		
+		System.out.println("스켈레톤이 "+power+"만큼 공격!");
+	}
 }
