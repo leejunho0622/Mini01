2291d1ea3ee752f72b6669e705921f674eb9b2c9
leejunho0622 <lee49409780@gmail.com>
2024-11-11 16:14:04 +0900

    Add Unit field, constructor and base methods

--- zombie/src/main/Unit.java
+++ zombie/src/main/Unit.java
@@ -1,5 +1,53 @@
 package main;
 
-public class Unit {
+abstract public class Unit {
+	private int hp;
+	private int mp;
+	private int level;
+	private int exp;
+	private int position;
 
+	public Unit(int hp, int mp, int level, int exp, int position) {
+		this.hp = hp;
+		this.mp = mp;
+		this.level = level;
+		this.exp = exp;
+		this.position = position;
+	}
+
+	public void setPos() {
+
+	}
+
+	public void setHp() {
+
+	}
+
+	public void setMp() {
+
+	}
+
+	public void levelUP() {
+
+	}
+
+	public void getPos() {
+
+	}
+
+	public void getHp() {
+
+	}
+
+	public void getMp() {
+
+	}
+
+	public void getLevel() {
+
+	}
+
+	public void move() {
+
+	}
 }
