c30367035f7fe918e43cf33219f8ceba0d90d8f2
leejunho0622 <lee49409780@gmail.com>
2024-11-11 17:03:02 +0900

    Add Boss constructor and attack method

--- zombie/src/Monster/Boss.java
+++ zombie/src/Monster/Boss.java
@@ -1,5 +1,23 @@
 package Monster;
 
-public class Boss {
+import java.util.Random;
 
+import main.Unit;
+
+public class Boss extends Unit{
+	private int power;
+	
+	public Boss(int hp, int mp, int level, int exp, int position) {
+		super(hp, mp, level, exp, position);
+	}
+	
+	public void attack(Unit player) {
+		Random ran = new Random();
+		power = ran.nextInt(this.level*5)+1;
+		player.setHp(player.getHp() - power);
+		if(player.getHp() < 0)
+			player.setHp(0);
+		
+		System.out.println("보스가 "+power+"만큼 공격!");
+	}
 }
