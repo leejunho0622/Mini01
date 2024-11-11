b2c6e07c34567b54cc418e36c9538e72d41598f5
leejunho0622 <lee49409780@gmail.com>
2024-11-11 17:10:23 +0900

    Add armor System in attack method

--- zombie/src/Monster/Skeleton.java
+++ zombie/src/Monster/Skeleton.java
@@ -5,6 +5,8 @@ import java.util.Random;
 import main.Unit;
 
 public class Skeleton extends Unit{
+	private int setPower;
+	private int armor;
 	private int power;
 	
 	public Skeleton(int hp, int mp, int level, int exp, int position) {
@@ -14,6 +16,9 @@ public class Skeleton extends Unit{
 	public void attack(Unit player) {
 		Random ran = new Random();
 		power = ran.nextInt(this.level*3)+1;
+		power = setPower - armor;
+		if(power <= 0)
+			power = 1;
 		player.setHp(player.getHp() - power);
 		if(player.getHp() < 0)
 			player.setHp(0);
