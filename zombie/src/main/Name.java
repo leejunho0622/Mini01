d73ad71374d89b2096dfa1a1576bb0ed8ea753ab
leejunho0622 <lee49409780@gmail.com>
2024-11-12 14:56:57 +0900

    Add names

--- zombie/src/main/Name.java
+++ zombie/src/main/Name.java
@@ -1,6 +1,6 @@
 package main;
 
 public interface Name {
-	public static String[] prefix = {"못생긴", "거대한", "작은", "이상한", "재빠른", "강한", "나약한"};
+	public static String[] prefix = {"못생긴", "거대한", "작은", "이상한", "재빠른", "강한", "나약한", "레어한", "ERROR"};
 	public abstract String setName();
 }
