5ca8f9b5d8e30bc2ea62ceca1accda9ca074c844
leejunho0622 <lee49409780@gmail.com>
2024-11-12 12:43:49 +0900

    Refactor: Code refactoring

--- zombie/src/main/Stage.java
+++ zombie/src/main/Stage.java
@@ -9,40 +9,40 @@ import Player.User;
 public class Stage {
 	private Random ran = new Random();
 	private Scanner scan = new Scanner(System.in);
-	
+
 	private int stage;
-	
+
 	private User player;
 	private Zombie zombie;
 	private Skeleton skeleton;
 	private Boss boss;
-	
+
 	private String name;
-	
+
 	public Stage(int stage) {
 		this.stage = stage;
 	}
-	
+
 	private void defaultEntity() {
 		player = new User(20, 100, 1, 0, 0);
 		zombie = new Zombie(10, 0, 1, 0, 3);
 		skeleton = new Skeleton(5, 0, 1, 0, 5);
 		boss = new Boss(40, 0, 3, 0, stage);
 	}
-	
+
 	private void move() {
-		player.setPos(player.getPos()+1);
+		player.setPos(player.getPos() + 1);
 	}
-	
+
 	private void levelUp() {
-		if(player.getExp() == 10) {
-			player.setHp(player.getHp() + player.getHp()/2);
+		if (player.getExp() == 10) {
+			player.setHp(player.getHp() + player.getHp() / 2);
 			player.setMp(player.getMp() + 30);
 			player.setExp(0);
 			player.levelUP();
 		}
 	}
-	
+
 	private void battleStart(Unit monster) {
 		System.out.println("야생의 적을 만났다!");
 		while (true) {
@@ -62,8 +62,8 @@ public class Stage {
 				System.out.println();
 				monster.attack(player);
 				System.out.println();
-			} 
-			if(battleEnd(monster))
+			}
+			if (battleEnd(monster))
 				break;
 		}
 	}
@@ -94,14 +94,14 @@ public class Stage {
 			battleStart(skeleton);
 		}
 	}
-	
-	private boolean isRun(int select){
-		if(select == 1)
+
+	private boolean isRun(int select) {
+		if (select == 1)
 			return true;
-		else 
+		else
 			return false;
 	}
-	
+
 	private int input(String msg) {
 		System.out.println(msg + "\n");
 		String input = scan.nextLine();
@@ -112,15 +112,15 @@ public class Stage {
 		}
 		return sel;
 	}
-	
+
 	public void run() {
 		defaultEntity();
-		while(stage > 0) {
+		while (stage > 0) {
 			levelUp();
-			if(isRun(input("[1] 이동 [2] 종료"))) {
+			if (isRun(input("[1] 이동 [2] 종료"))) {
 				move();
 				play();
-			}else
+			} else
 				break;
 			stage--;
 		}
