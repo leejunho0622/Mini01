078d284f2d4b76bf31bcde048d474893023e621b
leejunho0622 <lee49409780@gmail.com>
2024-11-12 16:01:41 +0900

    Implements UserStats

--- zombie/src/Player/User.java
+++ zombie/src/Player/User.java
@@ -3,7 +3,7 @@ package Player;
 import main.Player;
 import main.Unit;
 
-public class User extends Player{
+public class User extends Player implements UserStats{
 	public User(double hp, int mp, int level, int exp, int position) {
 		super(hp, mp, level, exp, position);
 	}
@@ -20,11 +20,16 @@ public class User extends Player{
 		if(monster.getHp() < 0)
 			monster.setHp(0);
 		
-		if(this.mp >= 100)
-			this.mp = 100;
+		if(this.mp >= UserStats.MAX_MP)
+			this.mp = UserStats.MAX_MP;
 		else 
 			this.mp += 10;
 		System.out.println("[플레이어] > "+damage+"만큼 공격!");
+	}
+	
+	@Override
+	public int getMaxHp() {
+		return 20 + this.level * 10;
 	}
 	
 	public void skill(int scroll, Unit monster) {
@@ -36,7 +41,7 @@ public class User extends Player{
 		}else if(scroll == 2 && this.mp >= 30) {
 			System.out.println("힐!");
 			this.hp += 10;
-			int maxHp = 20 + this.level * 10;
+			int maxHp = getMaxHp();
 			if(this.hp >= maxHp)
 				this.hp = maxHp;
 			this.mp -= 30;
@@ -44,6 +49,5 @@ public class User extends Player{
 		}else {
 			System.out.println("마나가 없습니다.");
 		}
-			
 	}
 }
