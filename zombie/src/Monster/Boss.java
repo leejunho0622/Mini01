3ed9f65a49c354debfcc216ec1b5a1130db26cc6
leejunho0622 <lee49409780@gmail.com>
2024-11-12 10:20:56 +0900

    Modify extends class and Remove import random

--- zombie/src/Monster/Boss.java
+++ zombie/src/Monster/Boss.java
@@ -1,29 +1,25 @@
 package Monster;
 
-import java.util.Random;
-
+import main.Monster;
 import main.Unit;
 
-public class Boss extends Unit{
-	private int setPower;
-	private int armor;
-	private int power;
-	
+public class Boss extends Monster{
 	public Boss(int hp, int mp, int level, int exp, int position) {
 		super(hp, mp, level, exp, position);
 	}
 	
 	@Override
 	public void attack(Unit player) {
-		Random ran = new Random();
-		setPower = ran.nextInt(this.level*5)+1;
-		power = setPower - armor;
-		if(power <= 0)
-			power = 1;
-		player.setHp(player.getHp() - power);
+		MonsterPower = ran.nextInt(this.level*5)+1;
+		armor = player.getLevel()/2;
+		damage = MonsterPower - armor;
+		
+		if(damage <= 0)
+			damage = 1;
+		player.setHp(player.getHp() - damage);
 		if(player.getHp() < 0)
 			player.setHp(0);
 		
-		System.out.println("[보스] > "+power+"만큼 공격!");
+		System.out.println("[보스] > "+damage+"만큼 공격!");
 	}
 }
--- zombie/src/Monster/Zombie.java
+++ zombie/src/Monster/Zombie.java
@@ -1,30 +1,24 @@
 package Monster;
 
-import java.util.Random;
-
+import main.Monster;
 import main.Unit;
 
-public class Zombie extends Unit {
-	private int setPower;
-	private int armor;
-	private int power;
-	
+public class Zombie extends Monster {
 	public Zombie(int hp, int mp, int level, int exp, int position) {
 		super(hp, mp, level, exp, position);
 	}
 	
 	@Override
 	public void attack(Unit player) {
-		Random ran = new Random();
-		setPower = ran.nextInt(this.level*2)+1;
+		MonsterPower = ran.nextInt(this.level*2)+1;
 		armor = player.getLevel()/2;
-		power = setPower - armor;
-		if(power <= 0)
-			power = 1;
-		player.setHp(player.getHp() - power);
+		damage = MonsterPower - armor;
+		if(damage <= 0)
+			damage = 1;
+		player.setHp(player.getHp() - damage);
 		if(player.getHp() < 0)
 			player.setHp(0);
 		
-		System.out.println("[좀비] > "+power+"만큼 공격!");
+		System.out.println("[좀비] > "+damage+"만큼 공격!");
 	}
 }
