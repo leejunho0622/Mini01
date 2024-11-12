ac831a850a56e08f8ec2804b4159689ffab8f2cd
leejunho0622 <lee49409780@gmail.com>
2024-11-12 10:38:03 +0900

    Add 부모 클래스 추가

---
+++ zombie/src/main/Player.java
@@ -0,0 +1,15 @@
+package main;
+
+import java.util.Random;
+
+abstract public class Player extends Unit{
+	protected Random ran = new Random();
+	protected int playerPower;
+	protected int armor;
+	protected int damage;
+	
+	protected Player(int hp, int mp, int level, int exp, int position) {
+		super(hp, mp, level, exp, position);
+	}
+	
+}
